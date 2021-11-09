89e1468e738b2d6d937ac07b6d8fe30a85ab1919
Manubeco <emanuelbeco@outlook.com>
2021-11-09 13:49:34 -0300

    primeiros comandos

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let x=100;
+let y=100;
+
 function DrawBall(x, y, r){
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2*Math.PI);
@@ -11,7 +14,19 @@ function DrawBall(x, y, r){
 }
 
 canvas.addEventListener('keydown', function(e){
-    console.log(e );
+   if(e.keyCode === 37){ 
+       x--;
+   }
+   if(e.keyCode === 38){ 
+       y--;
+   }
+   if(e.keyCode === 39){ 
+        x++;
+    }
+    if(e.keyCode === 40){ 
+        y++;
+    }
+
     })
     
-DrawBall(100, 100, 60); 
+DrawBall(x, y, 60); 
