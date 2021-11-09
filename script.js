a25b156ae23515a6f449db5e9c4ad4f743b6403e
manubeco <emanuelbeco@outlook.com>
2021-11-08 22:36:45 -0300

    segunda parte

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-console.log("JS works");
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getcontext('2d');
+const ctx = canvas.getContext('2d');
+
+function DrawBall(x, y, r){
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2*Math.PI);
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+    ctx.fillStyle = "red";  
+    ctx.fill();
+}
+
+canvas.addEventListener('keydown', function(e){
+    if(e.keyCode == 37){ 
+        console.log("LEFT");
+    }
+    
+DrawBall(100, 100, 60);
