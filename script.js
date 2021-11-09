21bab6e7b4424a4f45fb61ddd5692cd126995f0e
Manubeco <emanuelbeco@outlook.com>
2021-11-09 14:42:48 -0300

    movimento completo

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const ctx = canvas.getContext('2d');
 
 let x=100;
 let y=100;
+let LEFT, UP, RIGHT, DOWN; 
 
 function DrawBall(x, y, r){
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2*Math.PI);
-    ctx.strokeStyle = "red";
+    ctx.strokeStyle = "orange";
     ctx.stroke();
     ctx.fillStyle = "red";  
     ctx.fill();
@@ -15,18 +16,52 @@ function DrawBall(x, y, r){
 
 canvas.addEventListener('keydown', function(e){
    if(e.keyCode === 37){ 
-       x--;
+       LEFT = true;
    }
    if(e.keyCode === 38){ 
-       y--;
+       UP = true;
    }
    if(e.keyCode === 39){ 
-        x++;
+        RIGHT = true;
     }
     if(e.keyCode === 40){ 
+        DOWN = true;
+    }
+}); 
+
+canvas.addEventListener('keyup', function(e){
+    if(e.keyCode === 37){ 
+        LEFT = false;
+    }
+    if(e.keyCode === 38){ 
+        UP = false;
+    }
+    if(e.keyCode === 39){ 
+         RIGHT = false;
+     }
+     if(e.keyCode === 40){ 
+         DOWN = false;
+     }    
+})
+
+function move(){
+    if(LEFT){
+        x--;
+    }
+    if(UP){
+        y--;
+    }
+    if(RIGHT){
+        x++;
+    }
+    if(DOWN){
         y++;
     }
-
-    })
-    
-DrawBall(x, y, 60); 
+}
+function movimento() {
+    ctx.clearRect(0,0,canvas.clientWidth, canvas.clientHeight);
+    move();
+    DrawBall(x, y, 60)
+    requestAnimationFrame(movimento);
+}
+requestAnimationFrame(movimento);
